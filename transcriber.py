4e67eb7e76a0e4fe4d9e0ed1e773d3b72fefe879
AayushDeherkar <adeherkar@umass.edu>
2024-10-16 22:12:58 -0400

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -1,21 +1,23 @@
-#Author: Aayush Deherkar, Rashi Jain
+#Author: Aayush Deherkar, rashi Jain
 #Email: adeherkar@umass.edu, rashijain@umass.edu
-#Spire ID:34759120, 34559888
+#Spire ID:34759120 34559888
 import speech_recognition as sr
 
-def main():
+def main(audfile):
     aud = sr.Recognizer()
-    sound = "aud1.WAV"
+    sound = audfile
     
     #read the audio file
     with sr.AudioFile(sound) as source:
         print("Listening to the audio...")
-        audio_data = aud.record(source) 
+        audio_data = aud.record(source)  # Record the audio
     
     try:
-        # Convert the speech to text
+        #Speech to text
         text = aud.recognize_google(audio_data)
-        open 
         print("Converted audio is: " + text)
     except:
         print("file not found")
+
+
+main("aud1.WAV")
